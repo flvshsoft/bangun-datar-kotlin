cee7cffbce8e56feda1da3d644c16515576f7d52
flvshsoft <flashsoftindonesia@gmail.com>
2025-11-22 13:46:40 +0700

    first commit

---
+++ app/src/main/java/com/flashsoftindonesia/bangundatar/PersegiActivity.kt
@@ -0,0 +1,20 @@
+package com.flashsoftindonesia.bangundatar
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PersegiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_persegi)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
